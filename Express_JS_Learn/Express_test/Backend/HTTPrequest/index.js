b5a6cd985b9f8d3176e464418ceff1c8e5eaa22f
Pavat <pavatnon@gmail.com>
2024-06-24 23:22:36 +0700

    creat_endpoint

--- Express_JS_Learn/Express_test/Backend/HTTPrequest/index.js
+++ Express_JS_Learn/Express_test/Backend/HTTPrequest/index.js
@@ -4,8 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res)=> {
-    res.send("Hello WWorld");
+    res.send('<h1>Hello</h1>');
 });
+app.get('/contect', (req,res)=> {
+    res.send('<h1>+66000000000</h1>');
+});
+app.get('/about_me', (req,res)=> {
+    res.send('<h1>my name is non</h1>');
+});
+
 
 app.listen(port, ()=>{
     console.log(`Server is started port: ${port}`);
