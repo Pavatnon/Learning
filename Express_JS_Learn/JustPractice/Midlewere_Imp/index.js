cac1d570b6f1def187187412f45ca399f1dd18f7
Pavat <pavatnon@gmail.com>
2024-06-25 03:39:50 +0700

    LoginProject

---
+++ Express_JS_Learn/JustPractice/Midlewere_Imp/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const _pathdir = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.sendFile(_pathdir + '/Public/index.html');
+});
+
+app.post('/submit', (req,res) => {
+    console.log(req.body);
+    
+});
+
+app.listen(port,(err) => {
+    if(err) throw err;
+    console.log(`Server is  started port: ${port}`);
+});
