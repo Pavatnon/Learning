f64227e435310987e60f46009925141119bb28d5
Pavat <pavatnon@gmail.com>
2024-06-24 23:01:42 +0700

    request

--- Express_JS_Learn/Express_test/Backend/HTTPrequest/index.js
+++ Express_JS_Learn/Express_test/Backend/HTTPrequest/index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get('/', (req,res)=> {
+    res.send("Hello WWorld");
+});
+
 app.listen(port, ()=>{
     console.log(`Server is started port: ${port}`);
 });
