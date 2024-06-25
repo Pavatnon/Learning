4b90947b345ac0381df298e51632c36b7973fe98
Pavat <pavatnon@gmail.com>
2024-06-25 15:47:08 +0700

    practice
    implement : practice midlewere
    implement : practice request
    implement : practice respon

--- Express_JS_Learn/Middleware_Practice/index3.js
+++ Express_JS_Learn/Middleware_Practice/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res,next){
+  console.log("REQUEST METHOD: " + req.method);
+  console.log("REQUEST URL: " + req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
--- Express_JS_Learn/Middleware_Practice/index4.js
+++ Express_JS_Learn/Middleware_Practice/index4.js
@@ -1,7 +1,24 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+app.post('/submit', (req, res) => {
+  const streetName = req.body.street;
+  const petName = req.body.pet;
+  console.log(streetName);
+  console.log(petName);
+  res.send('<h1>Band Name is: </h1>' + `<h2>StreetName: ${streetName} </h2> <h2>PetName: ${petName} </h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
