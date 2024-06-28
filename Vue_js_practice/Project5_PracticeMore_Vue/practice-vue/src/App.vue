67ebf9b77ff8b35f49e70de771125b4cf44f1411
Pavat <pavatnon@gmail.com>
2024-06-29 01:48:33 +0700

    tttttest

---
+++ Vue_js_practice/Project5_PracticeMore_Vue/practice-vue/src/App.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <h1 class="test">{{title}}</h1>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        title: 'Hello, Vue'
+      }
+    },
+      
+  }
+</script>
+
+
+
