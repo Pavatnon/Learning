94c2973851688d09982eab4b57c545c6068988b6
Pavat <pavatnon@gmail.com>
2024-07-02 22:50:29 +0700

    ้hmm

---
+++ Vue_js_practice/Project7_Axios/AxiosPractice/src/App.vue
@@ -0,0 +1,42 @@
+<template >
+  <MountTest />
+  
+</template>
+<script setup>
+  import axios from 'axios';
+  import {ref, onMounted} from 'vue';
+  import MountTest from './components/mountTest.vue'
+
+  const pokeApi = ref([]);
+  const pokeNameData = ref([]);
+  const pokeImgData = ref([]);
+
+  let count = ref(0);
+
+  const getPoke = async () => {
+    try{
+      const resPoke = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+      pokeApi.value = resPoke.data;
+      pokeNameData.value = resPoke.data.results.map(poke => ({
+        name: poke.name,
+        img: poke.url
+      }));
+
+    } catch(e){
+      console.error(e);
+    }
+  };
+
+  onMounted(() => {
+    getPoke();
+  });
+
+
+
+
+
+
+</script>
+<style>
+  
+</style>
---
+++ Vue_js_practice/Project7_Axios/AxiosPractice/src/components/mountTest.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ massage }}
+    </div>
+</template>
+<script setup>
+import { ref ,onMounted } from 'vue';
+const massage = ref('Loading....');
+
+onMounted(()=>{
+    setTimeout(()=>{
+        massage.value = 'Hello, Vue 3!';
+    },2000);
+});
+    
+</script>
